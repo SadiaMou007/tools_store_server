e2075d9ce5a558315cd17b5eab66becc39927e27
SadiaMou007 <sadia.bu.cse@gmail.com>
2022-05-26 04:48:50 -0700

    heroku

--- index.js
+++ index.js
@@ -26,6 +26,31 @@ async function run() {
     //console.log("db connected");
     const productCollection = client.db("tools_store").collection("products");
     const bookingCollection = client.db("tools_store").collection("booking");
+    const userCollection = client.db("tools_store").collection("user");
+
+    //create/update user
+    app.put("/user/:email", async (req, res) => {
+      const email = req.params.email;
+
+      const user = req.body;
+      const filter = { email: email };
+      const options = { upsert: true };
+      const updatedDoc = { $set: user };
+      const result = await userCollection.updateOne(
+        filter,
+        updatedDoc,
+        options
+      );
+      //jwt 1(create)
+      const token = jwt.sign(
+        { email: email },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+          expiresIn: "1d",
+        }
+      );
+      res.send({ result, token });
+    });
 
     //get all products
     app.get("/products", async (req, res) => {
