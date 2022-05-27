7cea2a64f717901dba95cdc3469e4f15f2107d26
SadiaMou007 <sadia.bu.cse@gmail.com>
2022-05-26 18:33:43 -0700

    h

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const jwt = require("jsonwebtoken");
 
 require("dotenv").config();
 const app = express();
-app.use(cors());
+//app.use(cors());
 const port = process.env.PORT || 5000;
 
 app.use(
@@ -16,7 +16,6 @@ app.use(
     credentials: true,
   })
 );
-
 app.use(express.json());
 
 /////
